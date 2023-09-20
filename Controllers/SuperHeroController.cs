f185d8cb59b5d37c18bc285c46228bfe9e0acba8
maiyq1 <mijael.yq2002@outlook.com>
2023-09-19 20:52:41 -0500

    feat: HttpDelete

--- Controllers/SuperHeroController.cs
+++ Controllers/SuperHeroController.cs
@@ -52,5 +52,27 @@ namespace Test2.Controllers
             superHeroes.Add(newHero);
             return Ok(superHeroes);
         }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult<List<SuperHero>>> UpdateHero(int id, SuperHero request)
+        {
+            var hero = superHeroes.Find(x => x.Id == id);
+            if (hero is null) return NotFound("Sorry, but this hero doesn't exist.");
+            hero.Name = request.Name;
+            hero.FirstName = request.FirstName;
+            hero.LastName = request.LastName;
+            hero.Place = request.Place;
+
+            return Ok(superHeroes);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<List<SuperHero>>> DeleteHero(int id)
+        {
+            var hero = superHeroes.Find(x => x.Id == id);
+            if (hero is null) return NotFound("Sorry, but this hero doesn't exist.");
+            superHeroes.Remove(hero);
+            return Ok(superHeroes);
+        }
     }
 }
